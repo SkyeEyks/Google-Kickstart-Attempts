154eaff8ad8611c5b68f405b290ebc3072abe6e9
FlamboD <FlamboD2000@gmail.com>
2021-03-13 00:58:46 +0200

    Finished 2020/Round A/Plates

--- 2020/Round A/Plates.py
+++ 2020/Round A/Plates.py
@@ -2,17 +2,28 @@ from GoogleInput import GoogleInput
 
 
 def main():
+    def rec(idx: int, p: int):
+        if p == 0:
+            return 0
+        elif idx == N:  # Not enough plates taken
+            return -9e9
+        ans = -9e9
+        for i in range(min(K, p) + 1):
+            ans = max(ans,  rec(idx + 1, p - i) + (addedValues[idx][i-1] if i else 0))  # Added last part because when i was 0 it took the last value in addedValues[idx] and added it to the answer
+        return ans
+
     T = int(input())
 
     for case in range(1, T+1):
         N, K, P = map(int, input().split(" "))
-        plates = []
+        addedValues = []
         for stack in range(N):
-            plates.append([*map(int, input().split(" "))])
-        print(*plates, sep="\n")
+            nums = [*map(int, input().split(" "))]
+            addedValues.append([sum(nums[:i+1]) for i in range(K)])
+        print("Case #{}: {}".format(case, rec(0, P)))
 
 
-INPUT = GoogleInput(
+GoogleInput(
     "Plates - input.txt",
     "Plates - output.txt",
     main
